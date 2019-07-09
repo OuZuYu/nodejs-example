9049b8cf8b18d7eb629c8659e32f6bbe50e4a6d3
ouzuyu <oupenguin@gmail.com>
2019-07-09 15:21:33 +0800

    feat: add http

--- http.js
+++ http.js
@@ -1,5 +1,6 @@
 const http = require('http')
-const server = http.createServer((req, res) => {
+const fs = require('fs')
+/* const server = http.createServer((req, res) => {
   console.log('req :', req)
   console.log('res :', res)
 
@@ -12,4 +13,21 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen('1111')
-console.log('localhost:1111')
+console.log('localhost:1111') */
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-type': 'text/html'
+  })
+  fs.createReadStream('./static/index.html').pipe(res)
+
+  /* res.writeHead(200, {
+    'Content-type': 'application/json'
+  })
+  fs.createReadStream('./static/data.json').pipe(res) */
+
+  /* res.writeHead(200, {
+    'Content-type': 'image/jpg'
+  })
+  fs.createReadStream('./static/img.jpg').pipe(res) */
+}).listen(1111)
